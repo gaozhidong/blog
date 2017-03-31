90ad929d02eacd951523f5f9a6542f85c859c0c1
gaozhidong <1011284295@qq.com>
2017-03-31 09:38:33 +0800

    change advance-task15 demo-2

--- advance-task15/demo-1/index.js
+++ advance-task15/demo-1/index.js
@@ -1,44 +1,40 @@
 
-
 $('.btn').on('click', function () {
-    $(this).animate({ backgroundColor: "#FF0000" }, 1000, null, function () {
-        $('.btn').css("backgroundColor", "#FF0000");
-    });
-    $(this).animate({ backgroundColor: "#FF0000" }, 1000, null, function () {
-        $('.btn').css("backgroundColor", "#0000FF");
-    });
+    $(this).css({ 'background-color': 'red' });
+    setTimeout(function () {
+        $('.btn').css({ 'background-color': 'blue' });
+    }, 500)
+})
+
+$('.rollbox').on('scroll', function () {
+    var roll = $('.rollbox').scrollTop()
+    $('.distance').text(roll + 'PX')
+})
+
+$('.discoloration').on('mouseenter', function () {
+    $(this).css({ 'background-color': 'red' });
+})
+$('.discoloration').on('mouseleave', function () {
+    $(this).css({ 'background-color': '#fff' });
+})
+
+$('.txt').on('focus', function () {
+    $(this).css({ 'outlineColor': 'blue' })
+})
+$('.txt').on('keydown', function () {
+    $(this).val($(this).val().toUpperCase());
 });
-
-
-$('.btn1').on('click', function () {
-    var $scrollTop = $(window).scrollTop();
-
-    $(this).next().html(" 当前滚动高度为:" + $scrollTop).css('color', "#FF0000");
+$('.txt').on('keyup', function () {
+    $(this).val($(this).val().toUpperCase());
 });
-
-
-$('#cc').on('mouseenter', function () {
-    $(this).css('background-color', "#FF0000");
-});
-$('#cc').on('mouseleave', function () {
-    $(this).css('background-color', "#0000ff");
-});
-
-$('.in').on('focus', function () {
-    $(this).css('border-color', '#0000ff');
-    var u = $(this).val().toUpperCase();
-    $(this).val(u);
-});
-
-$('.in').on('blur', function () {
-    $(this).css('border-color', '#000');
+$('.txt').on('focusout', function () {
     console.log($(this).val());
 });
 
-$('select').on('change', function () {
-    console.log($(this).val());
-});
-
+$('.choose').on('change', function () {
+    var choosetxt = $('.choose option:selected').text()
+    $('.choosetxt span').text(choosetxt)
+})
 
 
 
