1f770b3eeb369469631a1d49e339e2ccdc58da3c
gaozhidong <1011284295@qq.com>
2017-09-16 14:39:49 +0800

    wheels-tabs

---
+++ wheels/dom/dom.js
@@ -0,0 +1,39 @@
+let dom = {
+    on: function (element, eventType, selector, fn) {
+        element.addEventListener(eventType, e => {
+            let el = e.target
+            while (!el.matches(selector)) {
+                if (element === el) {
+                    el = null
+                    break
+                }
+                el = el.parentNode
+            }
+            el && fn.call(el, e, el)
+        })
+        return element
+    },
+    index: function (element) {
+        let siblings = element.parentNode.children
+        for (let index = 0; index < siblings.length; index++) {
+            if (siblings[index] === element) {
+                return index
+            }
+        }
+        return -1
+    },
+    uniqueClass: function (element, className) {
+        dom.every(element.parentNode.children, el => {
+            el.classList.remove(className)
+        })
+        element.classList.add(className)
+        return element
+    },
+    every: function(nodeList, fn) {
+        for (var i = 0; i < nodeList.length; i++) {
+          fn.call(null, nodeList[i], i)
+        }
+        return nodeList
+      },
+
+}
